246642e3592d602d27ede9c584f467eca837cace
ThiagoSTeixeira <tteixeira.st@gmail.com>
2020-02-27 17:26:20 -0300

    aula3

--- aula3.cpp
+++ aula3.cpp
@@ -2,4 +2,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main();
+int main(){
+    long long n;
+    int k;
+    cin>> n;
+    cin >> k;
+    for(int i = 0; i<k; i++){
+        if(n%10 != 0) n -=1;
+        else n = n/10;
+    }
+    cout<< n;
+    return 0;
+}
