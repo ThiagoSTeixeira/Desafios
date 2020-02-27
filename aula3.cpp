60b0782d487cf38b97c0cc247a02b6e85a6d42ea
ThiagoSTeixeira <tteixeira.st@gmail.com>
2020-02-27 17:17:28 -0300

    completa solucaoAula2

---
+++ aula3.cpp
@@ -0,0 +1,5 @@
+//Problema A
+#include<bits/stdc++.h>
+using namespace std;
+
+int main();
--- solucaoAula2.cpp
+++ solucaoAula2.cpp
@@ -22,8 +22,60 @@ int main(){
 }
 
 //Problema D
-//pegar a foto dps (palindrome)
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check_palindrome(int l, int r, string& s){
+    while( l <- r && s[l] == s[r]) l++; r --;
+    if(l>r) return true;
+    return false;
+}
+int main(){
+    string s;
+    int k, n;
+    cin >> s >> k;
+    n = s.size();
+    if (n % k){
+        cout<<"NO\n";
+        return 0;
+    }
+    int sub = n/k;
+    for(int i =0; i<n; i+= sub){
+        if(!check_palindrome(i, i+sub-1, s)){
+            cout<<"NO\n";
+            return 0;
+        }
+    }
+    cout<<"YES\n";
+    return 0;
+}
+
 
 //Problema E
-//busca binaria? grafos?
-//Solucao nas fotos tambem
+//busca binaria? grafos
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string t, A, B, C;
+    int numA = 0, pos, n;
+    cin >> t;
+    n = t.size();
+    A=B=C="";
+
+    for(int i = 0; i< n; i++) numA += (t[i] == "(nao consegui anotar)")         //linha incompleta
+    if((n-numA) %2){
+        cout<<":(\n";
+        return 0;
+    }
+    pos = n - (n-numA)/2;
+    
+    for(int i=0; i<pos; i++) A+= t[i];
+    for(int i=pos; i<n; i++) B+= t[i];
+    for(int i=0; i<pos; i++) if(t[i] != 'a') algumacoisa C += t[i];         //linha incompleta
+
+    if(B == C) cout << A << '\n';
+    else cout<<":(\n";
+    
+    return 0;
+}
