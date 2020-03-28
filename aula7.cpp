37f7c62dc8d57aad45ac57dd417eaa4770e61be2
Thiago <tteixeira.st@gmail.com>
2020-03-28 01:08:41 -0300

    inicio aula 7

---
+++ aula7.cpp
@@ -0,0 +1,30 @@
+//Aula 7
+/*Problema A
+        -recebe x (2 <= x <= 10^9)
+        -encontrar 2 inteiros a e b tq maxDivCom(a,b) + minMultCom(a,b) = x
+        -caso haja mais de uma resposta, devolver qualquer uma
+        -t tests cases
+    Dicas:
+        -resolver na mao os casos de 2 a 10.
+        -tentar fazer em 1 linha talvez?
+        -ver a aula gravada
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    vector<int> x(t);
+    for (int i=0; i++; i<t) scanf("%d", &x[i]);
+
+    return(0);
+}
+/*Problema B
+    -array a de tamanho n (1 <= n <= 10^5 && 1 <= a <= 10^9)
+    -a soma dos n's nao excede 10^5
+    -qual sera a longest increasing subsequence de um array de n copias de a
+
+*/
+
+
