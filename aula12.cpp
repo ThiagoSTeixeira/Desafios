19a717ea199251b978a9958198df7102d969ed96
Thiago <tteixeira.st@gmail.com>
2020-04-15 13:49:08 -0300

    12B

--- aula12.cpp
+++ aula12.cpp
@@ -40,15 +40,30 @@ int main(){
 */
 #include<bits/stdc++.h>
 using namespace std;
-int getMax(int a1, int a2, int k1, int k2, int n){
-    while(n>0){
-        
-    }
-}
+
 int main(){
     int a1, a2, k1, k2, n;
+    int maxi, min;
     cin>>a1>>a2>>k1>>k2>>n;
-    
+    int maxB4Out = a1*(k1-1) + a2*(k2-1); //maximo de cartoes antes de haver expulsao
+    min = max(0, n-maxB4Out);
+    if(k1<=k2){
+        int cart = k1*a1;
+        if(n<cart) maxi = n/k1;
+        else{
+            n-=a1*k1;
+            maxi = a1 + n/k2;
+        }
+    }
+    if(k1>k2){
+        int cart = k2*a2;
+        if(n<cart) maxi = n/k2;
+        else{
+            n-=a2*k2;
+            maxi = a2 + n/k1;
+        }
+    }
+    cout<<min<<" "<<maxi;    
 }
 
 
