aea8604b6aa62e875252bc1ebbf0f2325c8fb171
Thiago <tteixeira.st@gmail.com>
2020-04-16 15:45:58 -0300

    habilidade apenas

--- aula11.cpp
+++ aula11.cpp
@@ -11,21 +11,13 @@ using namespace std;
 int main(){
     string palavra;
     bool sussu=false;
-    //getline(cin, entrada);
-    //istringstream ss(entrada);
     while(cin>>palavra){
-        //ss>>palavra;
-        //cout<<palavra<<endl;
         if(palavra == "Sussu" || palavra == "Sussu!"|| palavra == "Sussu.") sussu = true;
-        /*if (palavra.back() == '.' || palavra.back() == '!'){
-            cout<<"O cara é bom!"<<endl;
-            return 0;
-        }*/
     }
-    if (palavra.back() == '?'){
-            cout<<"7"<<endl;
-            return 0;
-        }
+    if(palavra.back() == '?'){
+        cout<<"7"<<endl;
+        return 0;
+    }
     if(sussu){
         cout<<"AI SUSSU!"<<endl;
         return 0;
@@ -33,3 +25,56 @@ int main(){
     cout<<"O cara é bom!"<<endl;
     return 0;
 }
+/*Problema B
+    -Mesa 10m x 10m
+    -Q querys (Q<= 5000)
+    -Query A = coloca prato cm centro em (x, y) e raio r
+    -Query B = remove o prato cm centro (x, y) e raio r
+    -0 <= x,y <=1000 && 1<=r<=1000
+    -Equacao da circunferencia-> (x-x0)^2 + (y-y0)^2 = r^2
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+bool encaixa(int x, int y, int r, int x0, int y0, int r0){
+    return(r0 == 0 || !(sqrt(pow(abs(x-x0), 2) + pow(abs(y-y0), 2)) < r+r0));
+}
+int main(){
+    int Q, tempX, tempY, tempR;
+    char type;
+    cin>>Q;
+    vector<int> x(Q, 0);
+    vector<int> y(Q, 0);
+    vector<int> r(Q, 0);
+    vector<bool> ans(Q, false);
+    for(int i=0; i<Q; i++){
+        cin>>type>>tempX>>tempY>>tempR;
+        if(type == 'R'){
+            for(int j=0; j<Q; j++){
+                if(x[j]==tempX && y[j]==tempY && r[j]==tempR){
+                    x[j]=y[j]=r[j]=0;
+                    ans[i] = true;
+                }
+            }
+        }
+        if(type == 'A'){
+            ans[i] = true;
+            for(int j=0; j<Q; j++){
+                if(!encaixa(tempX, tempY, tempR, x[j], y[j], r[j])){
+                    ans[i] = false;
+                    break;
+                }
+            }
+            if(ans[i]){
+                x[i] = tempX;
+                y[i] = tempY;
+                r[i] = tempR;
+            }
+        }
+    }
+    for(int i=0; i<Q; i++){
+        if(ans[i]) cout<<"Ok"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
+
